e7eb8899598c4545ed8ef54e56426c37c780f5d9
KadMir1 <zlobnisom@gmail.com>
2022-04-08 18:52:26 +0300

    first commit

---
+++ src/test/java/com/example/notesonspring/NotesonspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesonspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
